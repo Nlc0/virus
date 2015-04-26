d353dc801087450cf4c9e808195e2b1ea6dc0934
Nico <palardnicolas@gmail.com>
2015-04-26 17:04:24 +0200

    wups
    re add des fichiers

---
+++ Virus/src/GeneticStep.java
@@ -0,0 +1,17 @@
+package com.palardnicolasgmail.virus;
+
+public enum GeneticStep {
+	MOVE (0),
+	TURN_R (0),
+	TURN_L (0),
+	EAT (0),
+	CLONE (9);
+	
+	private final int waitTime;
+	
+	GeneticStep(int waitTime) {
+		this.waitTime = waitTime;
+	}
+	
+	public int waitTime() { return this.waitTime; }
+}
